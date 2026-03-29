0da61af47b10498d0e4b188e7927e267561ca6b8
kush088 <113185198+kush088@users.noreply.github.com>
2026-03-29 22:19:50 +0530

    Update cypress.config.js

--- cypresss/cypress.config.js
+++ cypresss/cypress.config.js
@@ -1,14 +1,8 @@
 const { defineConfig } = require("cypress");
-const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
 
 module.exports = defineConfig({
+  projectId: "eia7yr",
   e2e: {
-    setupNodeEvents(on, config) {
-      on('task', {downloadFile})
-      // implement node event listeners here
-      defaultCommandTimeout: 5000
-    },
-    "include": ["node_modules/cypress", "./cypress/**/*.js"]
+    baseUrl: "https://example.cypress.io", // 👈 no localhost needed
   },
-  
 });
